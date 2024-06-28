ee3c502a145b9aa69b12a84f8eea19a4828686d6
KatherineMedina55 <89563568+KatherineMedina55@users.noreply.github.com>
2024-06-28 14:46:47 -0500

    Update Ejercicio4.java

--- src/main/java/Ejercicios/Ejercicio4.java
+++ src/main/java/Ejercicios/Ejercicio4.java
@@ -5,37 +5,55 @@ import Framerwork.MapReduce;
 import Framerwork.NodoMap;
 import Framerwork.Tarea;
 import Framerwork.ParClaveValor;
+
 import java.util.ArrayList;
 
 /**
  *
- * @author andjo
+ * @author Katherine Medina
  */
+
 public class Ejercicio4 {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Tarea tarea1 = new Tarea();
         tarea1.setMapFunction(new NodoMap() {
-
+        
             @Override
             public void Map(ParClaveValor elemento, ArrayList<ParClaveValor> output) {
-                String[] line = elemento.getValor().toString().split(" ");
-                for (String item : line) {
-                    String[] lineData = item.split(",");
-                    if (!lineData[8].equals(lineData[12])) {
-                        output.add(new ParClaveValor(item, 1));
+                String[] lines = elemento.getValor().toString().split(" ");
+                
+                for (String line : lines) {
+                    String[] parts = line.split(",");
+
+                    if (parts.length >= 13) {
+                        String surface_Temp = parts[8];
+                        String wind_Chill = parts[12];
+                        String result = surface_Temp + ", " + wind_Chill;
+                        output.add(new ParClaveValor("Resultado: ", result));
                     }
                 }
             }
         });
+
         tarea1.setReduceFunction(new MapReduce() {
+
             @Override
             public void reduce(ParClaveValor elemento, ArrayList<ParClaveValor> output) {
-                output.add(new ParClaveValor(elemento.getClave(), ""));
+                @SuppressWarnings("unchecked")
+                ArrayList<String> list = (ArrayList<String>) elemento.getValor();
+                for (String item : list) {
+                    output.add(new ParClaveValor(elemento.getClave(), item));
+                }
             }
         });
+
+    
         tarea1.setInputFile("JCMB_last31days.csv");
         tarea1.setOutputfile("Ejercicio4.txt");
+
         tarea1.setNode(30);
+
         tarea1.run();
     }
 }
