0ac1feb44f4b66eaef265d6ad7289d2252751f10
MatildeSilvers <silversmatilde@gmail.com>
2024-06-27 22:07:33 -0500

    Ejercicio_2

--- src/main/java/Ejercicios/Ejercicio2.java
+++ src/main/java/Ejercicios/Ejercicio2.java
@@ -9,13 +9,13 @@ import Framerwork.NodoMap;
 import Framerwork.Tarea;
 import Framerwork.ParClaveValor;
 import java.util.ArrayList;
-
 /**
  *
  * @author andjo
  */
 public class Ejercicio2 {
-     static class GifMap  implements NodoMap  {
+    
+static class GifMap implements NodoMap {
         /**
          * Guarda en la lista output los ".gif" con un valor 1
          *
@@ -24,18 +24,17 @@ public class Ejercicio2 {
          */
         @Override
         public void Map(ParClaveValor tuplaEntrada, ArrayList<ParClaveValor> listaSalida) {
-            String[] palabras  = ((tuplaEntrada.getValor())).toString().split(" ");
-            for (String palabra  : palabras ) {
-                // Remove punctuation and convert to lowercase
-                String palabraLimpia  = palabra.toLowerCase();
-                if (palabraLimpia.contains(".gif"))// Compara si cada texto existe la ".gif2
+            String[] palabras = tuplaEntrada.getValor().toString().split(" ");
+            for (String palabra : palabras) {
+                if (palabra.toLowerCase().contains(".gif")) {
                     listaSalida.add(new ParClaveValor("gif", 1));
+                }
             }
         }
     }
 
-    static class SumReduce  implements MapReduce {
-       /**
+    static class SumReduce implements MapReduce {
+        /**
          * Cuenta cuántos ".gif" hay en la lista de tuplas ordenadas.
          *
          * @param tuplaClaveValores ParClaveValor que contiene la clave y la lista de valores asociados.
@@ -51,8 +50,8 @@ public class Ejercicio2 {
 
     public static void main(String[] args) {
         Tarea ejercicio2 = new Tarea();
-        ejercicio2.setReduceFunction(new SumReduce ());
-        ejercicio2.setMapFunction(new GifMap ());
+        ejercicio2.setReduceFunction(new SumReduce());
+        ejercicio2.setMapFunction(new GifMap());
         ejercicio2.setInputFile("weblog.txt");
         ejercicio2.setOutputfile("Ejercicio2.txt");
         ejercicio2.setNode(2);
